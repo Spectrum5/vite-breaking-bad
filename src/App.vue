636e2a85e019b13fb4bdc8dcea96aa7c9126720a
Sergio Tosku <toskusergio@gmail.com>
2023-02-06 16:26:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+import AppHero from "./components/AppHero.vue"
+
+export default {
+  components: {
+    AppTitle,
+    AppHero,
+  },
+
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <AppTitle />
+    <AppHero />
+  </div>
+</template>
+
+<style>
+
+body {
+  background-image: url('https://cdn.pixabay.com/photo/2012/08/25/22/22/saturn-54999_960_720.jpg');
+  background-size: cover;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!!',
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
